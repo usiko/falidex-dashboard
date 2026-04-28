dd0c0afe20ab31702ac105ea4f4bca2cfe9a48f0
quentin <quentin.usiko@gmail.com>
2026-04-28 18:12:41 +0200

    env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
     prod:true,
     configPaths:['/config/config.json'],
     tokenKey: "{ENV:TOKEN_HASH_KEY}",
-    token: "token",
     derivationTokenKey: "{ENV:DERIVATED_TOKEN_HASH_KEY}",
     tokenHeader: "X-Token"
 };
